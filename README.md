bc287be729344a53dd2d4672f4241825f3a57ae9
Cbaibai <chenyu122112121220@gmail.com>
2018-12-07 12:33:13 +0800

    小程序码

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-#### 公众号【摆摆笔记】关联的小程序-![](https://cbaibai.github.io/images/baibainote-weixin.png)摆摆笔记
+### 公众号【摆摆笔记】关联的小程序-摆摆笔记
+
+![摆摆笔记小程序码](http://pjavgi0cp.bkt.clouddn.com/%E6%91%86%E6%91%86%E7%AC%94%E8%AE%B0-%E5%B0%8F%E7%A8%8B%E5%BA%8F%E7%A0%81.jpg)
 
--- pages/RichText/index.js
+++ pages/RichText/index.js
@@ -1,6 +1,31 @@
 // pages/RichText/index.js
 Page({
-  data: {},
+  data: {
+    scanResult: ""
+  },
   onLoad: function() {
+
+  },
+  scan: function() {
+    wx.scanCode({
+      success: (res) => {
+        console.log("扫码结果: " + res.result);
+        this.setData({
+          scanResult: res.result
+        })
+      },
+      fail: (res) => {
+        console.log(res);
+      }
+    })
+  },
+
+  toast: function(){
+    wx.showToast({
+      title: '成功',
+      icon: 'succes',
+      duration: 2000,
+      mask: true
+    })
   }
 })
